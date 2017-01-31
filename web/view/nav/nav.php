3abaaba64716a48f5f738916a17c17eb052ce4fe
Gspohu <pohuvalentin@gmail.com>
2017-01-31 17:42:16 +0100

    Add base to nav menu HTML + CSS

--- web/view/nav/nav.php
+++ web/view/nav/nav.php
@@ -1,3 +1,25 @@
-<nav>
-nav
-<nav>
+<div class="nav">
+	<div class="navBlock">
+		<div class="navResultSergeLogo">
+		</div>
+		<a class="navTitle" href="result">
+			Your monitoring result
+		</a>
+	</div>
+
+	<div class="navBlock">
+		<div class="navSergeSettingLogo">
+		</div>
+		<a class="navTitle" href="setting">
+			Serge setting
+		</a>
+	</div>
+
+	<div class="navBlock">
+		<div class="navWikiLogo">
+		</div>
+		<a class="navTitle" href="wiki">
+			Wiki
+		</a>
+	</div>
+</div>
