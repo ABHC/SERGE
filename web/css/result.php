710e021b892ba3e9a9ee34eab6dccad0c4a8e1d9
Gspohu <pohuvalentin@gmail.com>
2017-02-06 19:18:05 +0100

    Creation of css model for result page

--- web/css/result.php
+++ web/css/result.php
@@ -14,12 +14,141 @@ include_once('nav.php');
 
 .body
 {
-	position: relative;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 	min-height: 100vh;
-	margin-top: -75px;
-	padding-top: 75px;
+}
+
+.background
+{
+	position: fixed;
+	top: 0;
+	left: 0;
+	margin: 0;
+	width: 100%;
+	height: 100vh;
 	z-index: -1;
-	/*background: linear-gradient(to right, rgba(0, 191, 99, 0.35), rgba(0, 171, 187, 0.35));*/
+	background: url('../images/background/Skyscraper01.jpg') center no-repeat;
+	background-size: cover;
+	/*filter: blur(2px) grayscale(100%);*/
+}
+
+.subBackground
+{
+	position: fixed;
+	top: 0;
+	left: 0;
+	margin: 0;
+	width: 100%;
+	height: 100vh;
+	z-index: -1;
+	background-color: rgba(0, 0, 0, 0.2);
+}
+
+h1
+{
+	font-size: 30px;
+	color: #fff;
+	text-transform: uppercase;
+	font-weight: 300;
+	text-align: center;
+	margin-bottom: 15px;
+}
+
+table
+{
+	width:100%;
+	table-layout: fixed;
+	word-wrap: break-word;
+	background-color: rgba(0, 0, 0, 0.45);
+}
+
+.table-header
+{
+	width: 80%;
+	background-color: rgba(255,255,255,0.3);
+	border: 1px solid rgba(255,255,255,0.05);
+}
+
+.table-content
+{
+	width: 80%;
+	height: auto;
+	overflow-x: auto;
+	margin-top: 0px;
+	border: 1px solid rgba(255,255,255,0.1);
+	margin-bottom: 100px;
+}
+
+th
+{
+	width: 13%;
+	padding: 15px 0 15px 0;
+	text-align: left;
+	font-weight: 500;
+	font-size: 12px;
+	color: #fff;
+	text-transform: uppercase;
+	text-align: center;
+}
+
+th a
+{
+	text-decoration: none;
+	color: #fff;
+}
+
+th:nth-child(1)
+{
+	width: 27%;
+}
+
+th:last-child
+{
+	width: 8%;
+}
+
+td
+{
+	width: 13%;
+	padding: 15px 0 15px 0;
+	text-align: center;
+	vertical-align:middle;
+	font-weight: 300;
+	font-size: 12px;
+	color: #fff;
+	border-bottom: solid 1px rgba(255,255,255,0.1);
+}
+
+td:nth-child(1)
+{
+	width: 27%;
+	text-align: left;
+	padding-left: 5px;
+}
+
+td:last-child
+{
+	width: 8%;
+}
+
+a.wikiLogo
+{
+	width: 25px;
+	height: 25px;
+	cursor: pointer;
+}
+
+a.wikiLogo img
+{
+	width: 25px;
+	height: 25px;
+}
+
+tr:nth-child(2n)
+{
+	background-color: rgba(255,255,255,0.08);
 }
 
 <?php
--- web/view/body/result.php
+++ web/view/body/result.php
@@ -1,3 +1,127 @@
+<div class="background">
+	<div class="subBackground">
+	</div>
+</div>
 <div class="body">
-Test
+	<h1>Watch result</h1>
+	<div class="table-header">
+		<table cellpadding="0" cellspacing="0" border="0">
+			<thead>
+				<tr>
+					<th><a href="?orderBy=title">Title</a></th>
+					<th><a href="?orderBy=keyword">Keyword</a></th>
+					<th><a href="?orderBy=source">Source</a></th>
+					<th><a href="?orderBy=date">Date</a></th>
+					<th><a href="?orderBy=sent">Sent</a></th>
+					<th><a href="?orderBy=read">Read</a></th>
+					<th><a href="wiki">Wiki</a></th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+	<div class="table-content">
+		<table cellpadding="0" cellspacing="0" border="0">
+			<tbody>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+				<tr>
+					<td>Les chiens sont'ils vos ancêtres ?</td>
+					<td>Génétique</td>
+					<td>Leschiensontdesoreilles.net</td>
+					<td>15/09/2049</td>
+					<td>Sent</td>
+					<td>Not read</td>
+					<td><a href="link?link" class="wikiLogo"><img src="../images/iconWikiWB.png"/></a></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
