8698c8f6533caad137e49122841e393e8519031f
Gspohu <pohuvalentin@gmail.com>
2017-03-07 18:11:43 +0100

    Create setting and wiki page in controller with variable for dynamique nav

---
+++ web/controller/setting.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once('model/get_text.php');
+
+$setting="active";
+
+include_once('view/nav/nav.php');
+
+include_once('view/body/setting.php');
+
+include_once('view/footer/footer.php');
+
+?>
