15b828f4b360dadb611bf22fde2a96047e4ff9c3
Gspohu <pohuvalentin@gmail.com>
2017-03-15 18:25:27 +0100

    Cleanning database_demo

--- database_demo/result_science_serge.sql
+++ database_demo/result_science_serge.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 15 Décembre 2016 à 15:48
--- Version du serveur :  5.7.16-0ubuntu0.16.04.1
--- Version de PHP :  7.0.8-0ubuntu0.16.04.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `CairnDevices`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `result_science_serge`
---
 
 CREATE TABLE `result_science_serge` (
   `title` text COLLATE utf8mb4_bin NOT NULL,
@@ -36,6 +11,4 @@ CREATE TABLE `result_science_serge` (
   `owners` text COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET NAMES utf8mb4;
