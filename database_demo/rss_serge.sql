aaa86bb15fc55f22c3b98d5608f8b87fca18d067
ABHC <combe.alexandre@gmail.com>
2017-03-17 16:06:27 +0100

    rss-serge update in database demo

--- database_demo/rss_serge.sql
+++ database_demo/rss_serge.sql
@@ -2,12 +2,12 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE `rss_serge` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `link` text COLLATE utf8mb4_bin NOT NULL,
   `name` text COLLATE utf8mb4_bin DEFAULT NULL,
   `owners` text COLLATE utf8mb4_bin NOT NULL,
-  `active` int(11) NOT NULL,
-  PRIMARY KEY (id)
+  `etag` text COLLATE utf8mb4_bin DEFAULT NULL,
+  `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 SET NAMES utf8mb4;
