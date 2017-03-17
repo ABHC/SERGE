1ab4280bf0caceb4fe987414f82d9666e83dd93a
Gspohu <pohuvalentin@gmail.com>
2017-03-17 13:50:39 +0100

    Auto increment id in rss_table serge

--- database_demo/rss_serge.sql
+++ database_demo/rss_serge.sql
@@ -2,7 +2,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE `rss_serge` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `link` text COLLATE utf8mb4_bin NOT NULL,
   `name` text COLLATE utf8mb4_bin DEFAULT NULL,
   `owners` text COLLATE utf8mb4_bin NOT NULL,
