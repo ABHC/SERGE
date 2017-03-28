da1dd11fc541a5d5cb82951d893cbd2174fd81ed
Gspohu <pohuvalentin@gmail.com>
2017-03-28 11:47:53 +0200

    Remove delete keyword entry in order to fix bug result bound to bad keyword id, plus this functionality is useless

--- web/model/delKeyword.php
+++ web/model/delKeyword.php
@@ -13,25 +13,4 @@
 		'active' => $active,
 		'id' => $keywordIdAction));
 		$req->closeCursor();
-
-
-		# Check if keyword need to be delete
-		$req = $bdd->prepare('SELECT applicable_owners_sources FROM keyword_news_serge WHERE id = :id');
-		$req->execute(array(
-			'id' => $keywordIdAction));
-			$applicable_owners_sources = $req->fetch();
-			$req->closeCursor();
-
-			if (preg_match("/(\|[0-9]+:,)+\|/", $applicable_owners_sources['applicable_owners_sources']))
-			{
-				$req = $bdd->prepare('DELETE FROM keyword_news_serge WHERE id = :id');
-				$req->execute(array(
-					'id' => $keywordIdAction));
-					$req->closeCursor();
-
-				# Rebuild index primary key
-				$bdd->query('SET  @num := 0');
-				$bdd->query('UPDATE keyword_news_serge SET id = @num := (@num+1)');
-				$bdd->query('ALTER TABLE keyword_news_serge AUTO_INCREMENT = 1');
-			}
 ?>
