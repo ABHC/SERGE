44b2e8b5b33c824fd00fd16cd9bf178a74c3d476
ABHC <combe.alexandre@gmail.com>
2017-03-30 18:15:35 +0200

    Second indent bug fix

--- mailer.py
+++ mailer.py
@@ -87,7 +87,7 @@ def buildMail(user, user_id_comma, register, pydate, permission_news, permission
 			if ":all@" in word_and_attribute[0] :
 				split_for_all = word_and_attribute[0].split("@")
 
-	     		call_name = database.cursor()
+				call_name = database.cursor()
 				call_name.execute(query_sitename, (split_for_all[1], ))
 				sitename = call_name.fetchone()
 				call_name.close()
