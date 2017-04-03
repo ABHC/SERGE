23416bb6e21e37108ac33ab3b7f70c260b5f16f5
Gspohu <pohuvalentin@gmail.com>
2017-04-03 11:50:56 +0200

    Source management delete disable activate vol1

---
+++ web/model/delSource.php
@@ -0,0 +1,12 @@
+<?php
+	$sourceOwnerNEW = preg_replace("/,!*$sourceIdAction,/", ',', $owners);
+
+	$active = $activeForCurrentSource - 1;
+
+	$req = $bdd->prepare('UPDATE rss_serge SET owners = :owners, active = :active WHERE id = :id');
+	$req->execute(array(
+		'owners' => $sourceOwnerNEW,
+		'active' => $active,
+		'id' => $sourceIdAction));
+		$req->closeCursor();
+?>
