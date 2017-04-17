6836581c3c02e18181b46372b8b2c91cce7ab493
Gspohu <pohuvalentin@gmail.com>
2017-04-17 15:29:56 +0200

    Improve search engine

--- web/controller/result.php
+++ web/controller/result.php
@@ -14,9 +14,15 @@ $setting = '';
 $colOrder['read'] = 'Read';
 $colOrder['send'] = 'Send';
 $colOrder['DESC'] = '';
+$colOrder['title'] = '';
+$colOrder['source'] = '';
+$colOrder['date'] = '';
 $recordLink = '';
 $search = '';
-$ORDERBYorSEARCH = '';
+$searchBoolean = '';
+$ORDERBY = '';
+
+$SELECTRESULT = '(SELECT id, title, link, send_status, read_status, `date`, id_source, keyword_id FROM result_news_serge WHERE owners LIKE :user';
 
 include_once('model/delResult.php');
 
@@ -52,7 +58,7 @@ include_once('model/readOwnerKeyword.php');
 include_once('model/readResultKeywordName.php');
 
 $resultBase  = 0;
-$resultLimit = 15;
+$resultLimit = 500;
 
 # Page number
 if (isset($_GET['page']) AND preg_match("/^[0-9]+$/", htmlspecialchars($_GET['page'])))
@@ -65,86 +71,86 @@ if (isset($_GET['page']) AND preg_match("/^[0-9]+$/", htmlspecialchars($_GET['pa
 # Order results
 if (isset($_GET['orderBy']))
 {
-	$ORDERBYorSEARCH = htmlspecialchars($_GET['orderBy']);
-	if ($ORDERBYorSEARCH == 'title')
+	$ORDERBY = htmlspecialchars($_GET['orderBy']);
+	if ($ORDERBY == 'title')
 	{
 		$colOrder['title'] = '▾';
 		$colOrder['DESC'] = 'DESC';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'ORDER BY title';
+		$ORDERBY = 'ORDER BY title';
 	}
-	elseif ($ORDERBYorSEARCH == 'titleDESC')
+	elseif ($ORDERBY == 'titleDESC')
 	{
 		$colOrder['title'] = '▴';
 		$colOrder['DESC'] = '';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'ORDER BY title DESC';
+		$ORDERBY = 'ORDER BY title DESC';
 	}
-	elseif ($ORDERBYorSEARCH == 'source')
+	elseif ($ORDERBY == 'source')
 	{
 		$colOrder['source'] = '▾';
 		$colOrder['DESC'] = 'DESC';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'ORDER BY id_source';
+		$ORDERBY = 'ORDER BY id_source';
 	}
-	elseif ($ORDERBYorSEARCH == 'sourceDESC')
+	elseif ($ORDERBY == 'sourceDESC')
 	{
 		$colOrder['source'] = '▴';
 		$colOrder['DESC'] = '';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'ORDER BY id_source DESC';
+		$ORDERBY = 'ORDER BY id_source DESC';
 	}
-	elseif ($ORDERBYorSEARCH == 'date')
+	elseif ($ORDERBY == 'date')
 	{
 		$colOrder['date'] = '▾';
 		$colOrder['DESC'] = 'DESC';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'ORDER BY date';
+		$ORDERBY = 'ORDER BY date';
 	}
-	elseif ($ORDERBYorSEARCH == 'dateDESC')
+	elseif ($ORDERBY == 'dateDESC')
 	{
 		$colOrder['date'] = '▴';
 		$colOrder['DESC'] = '';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'ORDER BY date DESC';
+		$ORDERBY = 'ORDER BY date DESC';
 	}
-	elseif ($ORDERBYorSEARCH == 'read')
+	elseif ($ORDERBY == 'read')
 	{
 		$colOrder['read'] = 'Read';
 		$colOrder['DESC'] = 'DESC';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'AND read_status LIKE \'%' . $_SESSION['id'] .'%\' ORDER BY date DESC';
+		$ORDERBY = 'AND read_status LIKE \'%' . $_SESSION['id'] .'%\' ORDER BY date DESC';
 	}
-	elseif ($ORDERBYorSEARCH == 'readDESC')
+	elseif ($ORDERBY == 'readDESC')
 	{
 		$colOrder['read'] = 'Unread';
 		$colOrder['DESC'] = '';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'AND read_status NOT LIKE \'%' . $_SESSION['id'] . '%\' ORDER BY date DESC';
+		$ORDERBY = 'AND read_status NOT LIKE \'%' . $_SESSION['id'] . '%\' ORDER BY date DESC';
 	}
-	elseif ($ORDERBYorSEARCH == 'send')
+	elseif ($ORDERBY == 'send')
 	{
 		$colOrder['send'] = 'Send';
 		$colOrder['DESC'] = 'DESC';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'AND send_status LIKE \'%' . $_SESSION['id'] .'%\' ORDER BY date DESC';
+		$ORDERBY = 'AND send_status LIKE \'%' . $_SESSION['id'] .'%\' ORDER BY date DESC';
 	}
-	elseif ($ORDERBYorSEARCH == 'sendDESC')
+	elseif ($ORDERBY == 'sendDESC')
 	{
 		$colOrder['send'] = 'Not send';
 		$colOrder['DESC'] = '';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'AND send_status NOT LIKE \'%' . $_SESSION['id'] .'%\' ORDER BY date DESC';
+		$ORDERBY = 'AND send_status NOT LIKE \'%' . $_SESSION['id'] .'%\' ORDER BY date DESC';
 	}
 	else
 	{
@@ -152,29 +158,40 @@ if (isset($_GET['orderBy']))
 		$colOrder['DESC'] = '';
 
 		# WARNING sensitive variable [SQLI]
-		$ORDERBYorSEARCH = 'ORDER BY date DESC';
+		$ORDERBY = 'ORDER BY date DESC';
 	}
 }
-else
+elseif (empty($_GET['search']))
 {
 	$colOrder['date'] = '▴';
 	$colOrder['DESC'] = '';
 
 	# WARNING sensitive variable [SQLI]
-	$ORDERBYorSEARCH = 'ORDER BY date DESC';
+	$ORDERBY = 'ORDER BY date DESC';
 }
 
 # Search in result
 if (!empty($_GET['search']))
 {
 	$search = htmlspecialchars($_GET['search']);
+	$searchBoolean = preg_replace("/(^|\ )[a-zA-Z]{1,3}\ /", " ", $search);
+	$searchBoolean = preg_replace("/\ /", "* ", $searchBoolean);
+	$searchBoolean = preg_replace("/.$/", "$0*", $searchBoolean);
+	$searchBoolean = preg_replace("/^..*.$/", "($0$1)", $searchBoolean);
 
 	# WARNING sensitive variable [SQLI]
-	$ORDERBYorSEARCH = 'AND (MATCH(title, link) AGAINST (:search)) ' . $ORDERBYorSEARCH;
+	$QUERYRESULT =
+	$SELECTRESULT . ' AND MATCH(title, link) AGAINST (:search))
+	 UNION ' .
+	 $SELECTRESULT . ' AND MATCH(title, link) AGAINST (:searchBoolean IN BOOLEAN MODE)  LIMIT 10)
+	 UNION ' .
+	 $SELECTRESULT . ' AND MATCH(title, link) AGAINST (:search WITH QUERY EXPANSION) LIMIT 10)' .
+	 $ORDERBY;
 }
 else
 {
-	$ORDERBYorSEARCH = 'AND title NOT LIKE :search ' . $ORDERBYorSEARCH;
+	# WARNING sensitive variable [SQLI]
+	$QUERYRESULT = $SELECTRESULT . ' AND title NOT LIKE :search AND title NOT LIKE :searchBoolean) ' . $ORDERBY;
 }
 
 include_once('model/readOwnerResult.php');
--- web/model/readOwnerResult.php
+++ web/model/readOwnerResult.php
@@ -1,13 +1,15 @@
 <?php
 // Read in BDD the results for current owner
 $userId = '%,' . $_SESSION['id'] . ',%';
-$reqReadOwnerResults = $bdd->prepare("SELECT id, title, link, send_status, read_status, `date`, id_source, keyword_id FROM result_news_serge WHERE owners LIKE :user $ORDERBYorSEARCH LIMIT :base, :lim");
+$reqReadOwnerResults = $bdd->prepare("$QUERYRESULT");
 	$reqReadOwnerResults->bindValue('user', $userId, PDO::PARAM_STR);
 	$reqReadOwnerResults->bindValue('search', $search, PDO::PARAM_STR);
-	$reqReadOwnerResults->bindValue('base', $resultBase, PDO::PARAM_INT);
-	$reqReadOwnerResults->bindValue('lim', $resultLimit, PDO::PARAM_INT);
+	$reqReadOwnerResults->bindValue('searchBoolean', $searchBoolean, PDO::PARAM_STR);
+	#$reqReadOwnerResults->bindValue('base', $resultBase, PDO::PARAM_INT);
+	#$reqReadOwnerResults->bindValue('lim', $resultLimit, PDO::PARAM_INT);
 	$reqReadOwnerResults->execute();
 
 $readOwnerResults = $reqReadOwnerResults->fetchAll();
+$nbResults = $reqReadOwnerResults->rowCount();
 $reqReadOwnerResults->closeCursor();
 ?>
