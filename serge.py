c267a184c466873eed599aef6ae8075cd1265780
Gspohu <pohuvalentin@gmail.com>
2017-04-25 15:21:44 +0200

    Fix bug of database connector not update

--- serge.py
+++ serge.py
@@ -706,10 +706,7 @@ def databaseConnection():
 sys.excepthook = cemeteriesOfErrors
 
 ######### CONNEXION TO Serge DATABASE
-passSQL = open("permission/password.txt", "r")
-passSQL = passSQL.read().strip()
-
-database = MySQLdb.connect(host="localhost", user="Serge", passwd=passSQL, db="Serge", use_unicode=1, charset="utf8mb4")
+database = databaseConnection()
 
 ######### TIME VARIABLES DECLARATION
 now = time.time()                                 #NOW IS A TIMESTAMPS
@@ -771,6 +768,9 @@ procPatents.join()
 
 pool.close()
 pool.join()
+
+######### CONNEXION TO Serge DATABASE
+database = databaseConnection()
 
 ######### AFFECTATION
 logger_info.info("AFFECTATION")
