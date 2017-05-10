a6dd0d16f6a9ed11067738a6cfca51a6b31559e7
Gspohu <pohuvalentin@gmail.com>
2017-05-10 17:49:04 +0200

    Add column in rss_serge in order to store favicon

--- database_demo/rss_serge.sql
+++ database_demo/rss_serge.sql
@@ -2,13 +2,14 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE `rss_serge` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `link` text COLLATE utf8mb4_bin NOT NULL,
-  `name` text COLLATE utf8mb4_bin DEFAULT NULL,
-  `owners` text COLLATE utf8mb4_bin NOT NULL,
-  `etag` text COLLATE utf8mb4_bin DEFAULT NULL,
-  `active` int(11) NOT NULL,
-  PRIMARY KEY (id)
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`link` text COLLATE utf8mb4_bin NOT NULL,
+	`name` text COLLATE utf8mb4_bin DEFAULT NULL,
+	`favicon` blob,
+	`owners` text COLLATE utf8mb4_bin NOT NULL,
+	`etag` text COLLATE utf8mb4_bin DEFAULT NULL,
+	`active` int(11) NOT NULL,
+	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 SET NAMES utf8mb4;
