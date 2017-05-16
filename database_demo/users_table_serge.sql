adf49e17e4cc25e8e1ecc23a68e5b06d4c7d1dfc
Gspohu <pohuvalentin@gmail.com>
2017-05-16 13:20:27 +0200

    Fix bug in database demo : missing comma + space to tab

--- database_demo/users_table_serge.sql
+++ database_demo/users_table_serge.sql
@@ -2,22 +2,22 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE `users_table_serge` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `users` text COLLATE utf8mb4_bin NOT NULL,
-  `email` text COLLATE utf8mb4_bin NOT NULL,
-  `password` text COLLATE utf8mb4_bin NOT NULL,
-  `last_mail` int(11) DEFAULT NULL,
-  `send_condition` text COLLATE utf8mb4_bin NOT NULL,
-  `frequency` int(11) DEFAULT NULL,
-  `link_limit` int(11) DEFAULT NULL,
-  `selected_days` text COLLATE utf8mb4_bin DEFAULT NULL,
-  `selected_hour` int(11) DEFAULT NULL,
-  `mail_design` text COLLATE utf8mb4_bin NOT NULL,
-  `language` varchar(2) COLLATE utf8mb4_bin DEFAULT 'EN',
-  `record_read` BOOLEAN NOT NULL,
-  `history_lifetime` int(11) DEFAULT NULL,
-  `background_result` text COLLATE utf8mb4_bin NOT NULL
-  PRIMARY KEY (id)
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`users` text COLLATE utf8mb4_bin NOT NULL,
+	`email` text COLLATE utf8mb4_bin NOT NULL,
+	`password` text COLLATE utf8mb4_bin NOT NULL,
+	`last_mail` int(11) DEFAULT NULL,
+	`send_condition` text COLLATE utf8mb4_bin NOT NULL,
+	`frequency` int(11) DEFAULT NULL,
+	`link_limit` int(11) DEFAULT NULL,
+	`selected_days` text COLLATE utf8mb4_bin DEFAULT NULL,
+	`selected_hour` int(11) DEFAULT NULL,
+	`mail_design` text COLLATE utf8mb4_bin NOT NULL,
+	`language` varchar(2) COLLATE utf8mb4_bin DEFAULT 'EN',
+	`record_read` BOOLEAN NOT NULL,
+	`history_lifetime` int(11) DEFAULT NULL,
+	`background_result` text COLLATE utf8mb4_bin NOT NULL,
+	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 SET NAMES utf8mb4;
