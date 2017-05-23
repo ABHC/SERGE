371d2cd78cf4a82b95145886eae53724cda341a4
Gspohu <pohuvalentin@gmail.com>
2017-05-23 17:42:21 +0200

    Create pages for sources pack patents pack Or science pack

---
+++ web/controller/watchPack.php
@@ -0,0 +1,11 @@
+<?php
+
+//include_once('model/get_text.php');
+
+include_once('view/nav/nav.php');
+
+include_once('view/body/watchPack.php');
+
+include_once('view/footer/footer.php');
+
+?>
---
+++ web/view/body/watchPack.php
@@ -0,0 +1,4 @@
+<div class="background"></div>
+<div class="body">
+
+</div>
