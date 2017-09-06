3cdc698f995b5aadf6d77083a29a21671dcded46
Gspohu <pohuvalentin@gmail.com>
2017-09-06 12:26:22 +0200

    Improve dataprocessing by adding 2 new type of data

--- web/controller/dataProcessing.php
+++ web/controller/dataProcessing.php
@@ -29,6 +29,28 @@ if ((isset($_POST['nonce']) AND isset($_SESSION['nonce :' . $_POST['nonce']]) AN
 				$data[$varDetails[0]] = !empty(htmlspecialchars($_GET[$varDetails[1]])) ? $_GET[$varDetails[1]]: "";
 			}
 		}
+		elseif ($varDetails[3] === 'email')
+		{
+			if ($varDetails[2] === 'POST')
+			{
+				$data[$varDetails[0]] = !empty(filter_var($_POST[$varDetails[1]], FILTER_SANITIZE_EMAIL)) ? $_POST[$varDetails[1]]: "";
+			}
+			elseif ($varDetails[2] === 'GET')
+			{
+				$data[$varDetails[0]] = !empty(filter_var($_GET[$varDetails[1]], FILTER_SANITIZE_EMAIL)) ? $_GET[$varDetails[1]]: "";
+			}
+		}
+		elseif ($varDetails[3] === 'Az')
+		{
+			if ($varDetails[2] === 'POST')
+			{
+				$data[$varDetails[0]] = !empty(preg_replace("/[^A-Za-z]/", "", $_POST[$varDetails[1]])) ? $_POST[$varDetails[1]]: "";
+			}
+			elseif ($varDetails[2] === 'GET')
+			{
+				$data[$varDetails[0]] = !empty(preg_replace("/[^A-Za-z]/", "", $_GET[$varDetails[1]])) ? $_GET[$varDetails[1]]: "";
+			}
+		}
 	}
 
 	if (!empty($_POST['nonce']))
