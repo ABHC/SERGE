092737c46b31f7271d4c220287703fe9aa5298ab
Gspohu <pohuvalentin@gmail.com>
2017-09-06 12:30:17 +0200

    Cleaning model

--- web/controller/index.php
+++ web/controller/index.php
@@ -97,10 +97,26 @@ if($dataProcessing AND !empty($data['pseudo']) AND !empty($data['email']) AND !e
 					// Salt generation
 					$bytes = random_bytes(5);
 					$cryptoSalt = bin2hex($bytes);
-
 					$password = hash('sha256', $cryptoSalt . $data['password']);
-					include_once('model/signup.php');
-					$password = "";
+
+					// Insert new user in database
+					$insertCol = array(array("users", $pseudo),
+														array("email", $data['email']),
+														array("password", $password),
+														array("salt", $cryptoSalt),
+														array("signup_date", time()),
+														array("send_condition", 'link_limit'),
+														array("mail_design", 'masterword'),
+														array("record_read", 1),
+														array("background_result", 'Skyscrapers'),
+														array("token", $token));
+					$execution = insert('users_table_serge', $insertCol, '', '', $bdd);
+
+					// Read new user information in order to connect it
+					$checkCol = array(array("users", "=", $pseudo, "AND"),
+														array("password", "=", $password, ""));
+					$result = read('users_table_serge', 'id, users', $checkCol, '', $bdd);
+					$result = $result[0];					$password = "";
 
 					# Connection
 					session_start();
