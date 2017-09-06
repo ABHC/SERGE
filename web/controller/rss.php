7ea2744e433ba2e3b5e748d12a495bb2bdcf6a61
Gspohu <pohuvalentin@gmail.com>
2017-09-06 11:09:40 +0200

    Adapt RSS page to science an patents type of news

--- web/controller/rss.php
+++ web/controller/rss.php
@@ -1,6 +1,7 @@
 <?php
 include_once('model/read.php');
 
+$_GET['token'] = preg_replace("/\.php/", "", $_GET['token']);
 preg_match("/[A-Fa-f0]{8}/", $_GET['token'], $token);
 
 $checkCol = array(array("token", "=", $token[0], "AND"),
@@ -8,11 +9,31 @@ array("premium", "=" , 1, ""));
 $user = read('users_table_serge', 'id, users', $checkCol, '', $bdd);
 $user = $user[0];
 
+preg_match("/[sp]$/", $_GET['token'], $resultType);
+
 if (!empty($user))
 {
-	// read available results
-	$checkCol = array(array("owners", "l", ',' . $user['id'] . ',', ""));
-	$results = read("result_news_serge", 'title, link, date', $checkCol, 'ORDER BY date DESC LIMIT 30',$bdd);
+	if ($resultType[0] === 's')
+	{
+		// read available results
+		$checkCol = array(array("owners", "l", ',' . $user['id'] . ',', ""));
+		$results = read("result_science_serge", 'title, link, date', $checkCol, 'ORDER BY date DESC LIMIT 30',$bdd);
+		$description = 'Scientifique publication';
+	}
+	elseif ($resultType[0] === 'p')
+	{
+		// read available results
+		$checkCol = array(array("owners", "l", ',' . $user['id'] . ',', ""));
+		$results = read("result_patents_serge", 'title, link, date', $checkCol, 'ORDER BY date DESC LIMIT 30',$bdd);
+		$description = 'Patents';
+	}
+	elseif (empty($results))
+	{
+		// read available results
+		$checkCol = array(array("owners", "l", ',' . $user['id'] . ',', ""));
+		$results = read("result_news_serge", 'title, link, date', $checkCol, 'ORDER BY date DESC LIMIT 30',$bdd);
+		$description = 'General news';
+	}
 
 	if (!empty($recordLink))
 	{
@@ -25,7 +46,7 @@ if (!empty($user))
 	<channel>
 	<title>Serge : RSS ' . $user['users'] . '</title>
 	<link>http://serge.eu/</link>
-	<description></description>';
+	<description>' . $description . '</description>';
 
 	foreach ($results as $result)
 	{
