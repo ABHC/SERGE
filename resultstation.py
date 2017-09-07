78a9dddf2cf6b9a2ba8eb8bfa1bdf8bd929fe92f
Gspohu <pohuvalentin@gmail.com>
2017-09-07 17:29:39 +0200

    Apply new record read system to serge newsletter system

--- resultstation.py
+++ resultstation.py
@@ -9,7 +9,7 @@ from urllib import pathname2url as pn
 from handshake import databaseConnection
 
 
-def recorder(register, typeName, link, database):
+def recorder(register, typeName, linkId, database):
 	"""Creation of "recording links" that update Serge Database when clicked"""
 
 	query_domain = ("SELECT value FROM miscellaneous_serge WHERE name = 'domain'")
@@ -21,25 +21,16 @@ def recorder(register, typeName, link, database):
 
 	domain = domain[0]
 
-	query_user_secrets = ("SELECT users, password FROM users_table_serge WHERE id LIKE %s")
+	query_user_secrets = ("SELECT token FROM users_table_serge WHERE id = %s")
 
 	call_users = database.cursor()
 	call_users.execute(query_user_secrets, (register,))
-	user_secrets = call_users.fetchone()
+	token = call_users.fetchone()
 	call_users.close()
 
-	user_name = user_secrets[0]
-	user_pass = user_secrets[1]
+	token = token[0]
 
-	salt = "blackSalt"
-	chop = hashlib.sha256(salt + ":" + user_pass + user_name + str(register)).hexdigest()
-
-	try:
-		recording_link = "http://" + domain + "/redirect?id=" + str(register) + "&type=" + typeName + "&hash=" + chop + "&link=" + pn(link)
-	except Exception, except_type:
-		logger_error.error("ERROR IN RECORDING LINK CONTRUCTION ON :"+link+"\n")
-		logger_error.error(repr(except_type))
-		recording_link = link
+	recording_link = "http://" + domain + "/redirect?type=" + typeName + "&token=" + token + "&id=" + linkId
 
 	return (recording_link)
 
@@ -66,7 +57,7 @@ def triage(register, user_id_comma):
 	not_send_patents_list = []
 
 	######### RESULTS NEWS : NEWS ATTRIBUTES QUERY (LINK + TITLE + ID SOURCE + KEYWORD ID)
-	query_news = ("SELECT link, title, id_source, keyword_id FROM result_news_serge WHERE (send_status NOT LIKE %s AND owners LIKE %s)")
+	query_news = ("SELECT link, title, id_source, keyword_id, id FROM result_news_serge WHERE (send_status NOT LIKE %s AND owners LIKE %s)")
 
 	call_news = database.cursor()
 	call_news.execute(query_news, (user_id_comma, user_id_comma))
@@ -74,11 +65,11 @@ def triage(register, user_id_comma):
 	call_news.close()
 
 	for row in rows:
-		field = [row[0], row[1], row[2], str(row[3]), row[0]]
+		field = [row[0], row[1], row[2], str(row[3]), row[0], str(row[4])]
 		not_send_news_list.append(field)
 
 	######### RESULTS SCIENCE : SCIENCE ATTRIBUTES QUERY (LINK + TITLE + KEYWORD ID)
-	query_science = ("SELECT link, title, query_id, id_source FROM result_science_serge WHERE (send_status NOT LIKE %s AND owners LIKE %s)")
+	query_science = ("SELECT link, title, query_id, id_source, id FROM result_science_serge WHERE (send_status NOT LIKE %s AND owners LIKE %s)")
 
 	call_science = database.cursor()
 	call_science.execute(query_science, (user_id_comma, user_id_comma))
@@ -87,11 +78,12 @@ def triage(register, user_id_comma):
 
 	for row in rows:
 		row = list(row)
+		row.insert(5, row[4])
 		row.insert(4, row[0])
 		not_send_science_list.append(row)
 
 	######### RESULTS PATENTS : PATENTS ATTRIBUTES QUERY (LINK + TITLE + ID QUERY WIPO)
-	query_patents = ("SELECT link, title, id_query_wipo FROM result_patents_serge WHERE (send_status NOT LIKE %s AND owners LIKE %s)")
+	query_patents = ("SELECT link, title, id_query_wipo, id FROM result_patents_serge WHERE (send_status NOT LIKE %s AND owners LIKE %s)")
 
 	call_patents = database.cursor()
 	call_patents.execute(query_patents, (user_id_comma, user_id_comma))
@@ -100,27 +92,28 @@ def triage(register, user_id_comma):
 
 	for row in rows:
 		row = list(row)
+		row.insert(4, row[3])
 		row.insert(3, row[0])
 		not_send_patents_list.append(row)
 
 	######### LINKS MODIFICATION FOR RECORDS
 	if record_read is True:
 		for news in not_send_news_list:
-			link = news[0]
+			linkId = news[5]
 			typeName = "news"
-			recording_link = recorder(register, typeName, link, database)
+			recording_link = recorder(register, typeName, linkId, database)
 			news[0] = recording_link
 
 		for science in not_send_science_list:
-			link = science[0]
+			linkId = str(science[5])
 			typeName = "sciences"
-			recording_link = recorder(register, typeName, link, database)
+			recording_link = recorder(register, typeName, linkId, database)
 			science[0] = recording_link
 
 		for patent in not_send_patents_list:
-			link = patent[0]
+			linkId = str(patent[4])
 			typeName = "patents"
-			recording_link = recorder(register, typeName, link, database)
+			recording_link = recorder(register, typeName, linkId, database)
 			patent[0] = recording_link
 
 	return (not_send_news_list, not_send_science_list, not_send_patents_list)
