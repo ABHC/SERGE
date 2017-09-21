6eb7e069144e8d85f4f82cebf85dee67dd002ae6
Gspohu <pohuvalentin@gmail.com>
2017-09-21 11:51:31 +0200

    Cleaning + fix vuln : bad usage of sleep

--- web/controller/workinprogress.php
+++ web/controller/workinprogress.php
@@ -5,6 +5,11 @@ include('model/insert.php');
 include('view/nav/nav.php');
 include('controller/generateNonce.php');
 
+# Initialization of variables
+$resultTab    = '';
+$wikiTab      = '';
+$settingTab   = '';
+
 # Data processing
 $unsafeData = array();
 $unsafeData = array_merge($unsafeData, array(array('email', 'email', 'POST', 'email')));
@@ -14,18 +19,18 @@ include('controller/dataProcessing.php');
 
 # Nonce
 $nonceTime = $_SERVER['REQUEST_TIME'];
-$nonce = getNonce($nonceTime);
+$nonce     = getNonce($nonceTime);
 
 $deleveryTime = 1505260000 ;
-$timeLeft = $deleveryTime - $_SERVER['REQUEST_TIME'];
-$day = floor($timeLeft / (24*3600));
-$hour = floor(($timeLeft - ($day*24*3600)) / (3600));
-$minute = floor(($timeLeft - ($day*24*3600) - ($hour*3600)) / 60);
-$second = ($timeLeft - ($day*24*3600) - ($hour*3600) - ($minute*60));
+$timeLeft     = $deleveryTime - $_SERVER['REQUEST_TIME'];
+$day          = floor($timeLeft / (24*3600));
+$hour         = floor(($timeLeft - ($day*24*3600)) / (3600));
+$minute       = floor(($timeLeft - ($day*24*3600) - ($hour*3600)) / 60);
+$second       = ($timeLeft - ($day*24*3600) - ($hour*3600) - ($minute*60));
 
 if (!empty($data['newsletter']) AND !empty($data['email']))
 {
-	$checkCol = array(array('email', '=', $data['email'], ''));
+	$checkCol     = array(array('email', ' =', $data['email'], ''));
 	$result_email = read('newsletter_table_serge', '', $checkCol, '',$bdd);
 
 	if (empty($result))
@@ -34,7 +39,6 @@ if (!empty($data['newsletter']) AND !empty($data['email']))
 											array('signup_date', $_SERVER['REQUEST_TIME']));
 		$execution = insert('newsletter_table_serge', $insertCol, '', 'workinprogress', $bdd);
 	}
-	sleep(1);
 }
 
 include('view/body/workinprogress.php');
