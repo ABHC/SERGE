873310d962d98dd8fd8e7796bb1c025b8b86815c
Gspohu <pohuvalentin@gmail.com>
2017-09-22 11:34:45 +0200

    Update failsafe to database upgrade

--- failsafe.py
+++ failsafe.py
@@ -42,7 +42,7 @@ def checkMate():
 
 	num_tables = num_tables[0]
 
-	if num_tables < 18:
+	if num_tables < 23:
 		logger_error.critical("Missing Tables")
 	else:
 		call_extensions = database.cursor()
@@ -66,12 +66,14 @@ def checkMate():
 
 				optionnal_tables = optionnal_tables + amount_tables
 
-		if num_tables == (21+optionnal_tables):
+		if num_tables == (23 + optionnal_tables):
 			logger_info.info("Number of tables : check")
-		elif num_tables < (21+optionnal_tables):
+		elif num_tables < (23 + optionnal_tables):
+			logger_info.info("Missing Tables, for at least one extension")
 			logger_error.critical("Missing Tables, for at least one extension")
 			sys.exit()
-		elif num_tables > (21+optionnal_tables):
+		elif num_tables > (23 + optionnal_tables):
+			logger_info.info("Too Much Tables")
 			logger_error.critical("Too Much Tables")
 			sys.exit()
 		else:
@@ -80,7 +82,7 @@ def checkMate():
 			sys.exit()
 
 	######### CHECKING TABLES' NAMES
-	expected_tables_list = ["admin_table_serge", "background_serge", "keyword_news_serge", "language_serge", "patents_sources_serge", "premium_code_table_serge", "purchase_table_serge", "queries_science_serge", "queries_wipo_serge", "result_news_serge", "result_patents_serge", "result_science_serge", "rss_serge", "science_sources_serge", "text_content_serge", "miscellaneous_serge", "newsletter_table_serge", "users_table_serge", "watch_pack_queries_serge", "watch_pack_serge", "captcha_serge"]
+	expected_tables_list = ["admin_table_serge", "background_serge", "keyword_news_serge", "language_serge", "patents_sources_serge", "premium_code_table_serge", "price_table_serge", "purchase_table_serge", "queries_science_serge", "queries_wipo_serge", "result_news_serge", "result_patents_serge", "result_science_serge", "rss_serge", "science_sources_serge", "stripe_table_serge", "text_content_serge", "miscellaneous_serge", "newsletter_table_serge", "users_table_serge", "watch_pack_queries_serge", "watch_pack_serge", "captcha_serge"]
 
 	checking.execute(check_tables_name)
 	name_tables = checking.fetchall()
@@ -102,8 +104,9 @@ def checkMate():
 	keyword_news_serge_numbers = 4
 	language_serge_numbers = 2
 	patents_sources_serge_numbers = 3
-	premium_code_table_serge_numbers = 5
-	purchase_table_serge_numbers = 7
+	premium_code_table_serge_numbers = 6
+	price_table_serge_numbers = 4
+	purchase_table_serge_numbers = 8
 	queries_science_serge_numbers = 5
 	queries_wipo_serge_numbers = 5
 	result_news_serge_numbers = 10
@@ -111,10 +114,11 @@ def checkMate():
 	result_science_serge_numbers = 10
 	rss_serge_numbers = 7
 	science_sources_serge_numbers = 3
+	stripe_table_serge_numbers = 4
 	text_content_serge_numbers = 6
 	miscellaneous_serge_numbers = 2
 	newsletter_table_serge_numbers = 3
-	users_table_serge_numbers = 19
+	users_table_serge_numbers = 20
 	watch_pack_queries_serge_numbers = 4
 	watch_pack_serge_numbers = 10
 	captcha_serge_numbers = 1
@@ -149,8 +153,9 @@ def checkMate():
 	keyword_news_serge_columns = ["id", "keyword", "applicable_owners_sources", "active"]
 	language_serge_columns = ["code", "name"]
 	patents_sources_serge_columns = ["id", "link", "name"]
-	premium_code_table_serge_columns = ["id", "code", "creation_date", "users", "duration_premium"]
-	purchase_table_serge_columns = ["id", "user_id", "purchase_date", "duration_premium", "invoice_number", "price", "bank_details"]
+	premium_code_table_serge_columns = ["id", "code", "creation_date", "users", "duration_premium", "expiration_date"]
+	price_table_serge_columns = ["id", "price", "currency", "type"]
+	purchase_table_serge_columns = ["id", "user_id", "purchase_date", "duration_premium", "invoice_number", "price", "premium_code_id", "bank_details"]
 	queries_science_serge_columns = ["id", "query_arxiv", "query_doaj", "owners", "active"]
 	queries_wipo_serge_columns = ["id", "query", "legal_research", "owners", "active"]
 	result_news_serge_columns = ["id", "search_index", "title", "link", "send_status", "read_status", "date", "id_source", "keyword_id", "owners"]
@@ -158,10 +163,11 @@ def checkMate():
 	result_science_serge_columns = ["id", "search_index", "title", "link", "send_status", "read_status", "date", "id_source", "query_id", "owners"]
 	rss_serge_columns = ["id", "link", "name", "favicon", "owners", "etag", "active"]
 	science_sources_serge_columns = ["id", "link", "name"]
+	stripe_table_serge_columns = ["id", "account_name", "secret_key", "publishable_key"]
 	text_content_serge_columns = ["index_name", "EN", "FR", "ES", "DE", "CN"]
 	miscellaneous_serge_columns = ["name", "value"]
 	newsletter_table_serge_columns = ["id", "email", "signup_date"]
-	users_table_serge_columns = ["id", "users", "email", "password", "salt", "signup_date", "last_mail", "send_condition", "frequency", "link_limit", "selected_days", "selected_hour", "mail_design", "language", "record_read", "history_lifetime", "background_result", "premium", "token"]
+	users_table_serge_columns = ["id", "users", "email", "password", "salt", "signup_date", "last_mail", "send_condition", "frequency", "link_limit", "selected_days", "selected_hour", "mail_design", "language", "record_read", "history_lifetime", "background_result", "premium", "expiration_date", "token"]
 	watch_pack_queries_serge_columns = ["id", "pack_id", "query", "source"]
 	watch_pack_serge_columns = ["id", "search_index", "name", "description", "author", "users", "category", "language", "update_date", "rating"]
 	captcha_serge_columns = ["name"]
