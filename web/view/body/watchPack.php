927e809740176232287789372184a7cadb3765c5
Gspohu <pohuvalentin@gmail.com>
2017-11-06 17:01:30 +0100

    Cleaning

--- web/view/body/watchPack.php
+++ web/view/body/watchPack.php
@@ -34,7 +34,6 @@
 			echo '0';
 		}
 		?>"/>
-		<input type="hidden" name="scrollPos" id="scrollPos" value="0"/>
 		<input type="hidden" name="delEditingScienceQuery" value="<?php echo $delEditingScienceQuery ?? ''; ?>"/>
 		<input type="hidden" name="delEditingPatentQuery" value="<?php echo $delEditingPatentQuery ?? ''; ?>"/>
 
