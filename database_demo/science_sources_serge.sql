2d81b696a9d6d54d850e6ee7fbb866cd12aef141
ABHC <combe.alexandre@gmail.com>
2018-01-09 18:50:19 +0100

    add PLOS

--- database_demo/science_sources_serge.sql
+++ database_demo/science_sources_serge.sql
@@ -14,4 +14,5 @@ INSERT INTO `science_sources_serge` (`id`, `link`, `name`) VALUES
 (1, 'http://arxiv.org/', 'ArXiv.org'),
 (2, 'https://doaj.org/', 'Directory of Open Access Journals (DOAJ)'),
 (3, 'http://api.archives-ouvertes.fr', 'HAL - Archives Ouvertes'),
-(4, 'https://api.elsevier.com', 'Science Direct (Elsevier)');
+(4, 'http://journals.plos.org', 'PLOS'),
+(5, 'https://api.elsevier.com', 'Science Direct (Elsevier)');
