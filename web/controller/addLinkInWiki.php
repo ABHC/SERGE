e455acc7f8b0f44926f3c36820f3f9b594fc5270
Gspohu <pohuvalentin@gmail.com>
2018-01-24 19:02:37 +0100

    Creation of error page for adding link in wiki

---
+++ web/controller/addLinkInWiki.php
@@ -0,0 +1,18 @@
+<?php
+
+# Define variable
+$resultTab             = '';
+$wikiTab               = '';
+$settingTab            = '';
+
+include('model/get_text.php');
+include('model/get_text_var.php');
+
+
+include('view/nav/nav.php');
+
+include('view/body/addLinkInWiki.php');
+
+include('view/footer/footer.php');
+
+?>
---
+++ web/view/body/addLinkInWiki.php
@@ -0,0 +1,7 @@
+<div class="background"></div>
+<div class="body">
+	<div class="window">
+		<div class="emoticon"></div>
+		<?php get_t('Sorry, the wiki has not yet been released', $bdd); ?>
+	</div>
+</div>
