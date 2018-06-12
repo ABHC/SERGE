bbb612660111a04683de3486b43ee6216346232a
Ruffenach <ruffenach.jonathan@gmail.com>
2018-06-12 19:38:05 +0200

    adding Adminer

---
+++ Adminer.php
@@ -0,0 +1,9 @@
+<?php
+/******Adminer******/
+
+/*Sauvegarde de la bdd*/
+//Lecture du fichier de config
+//Back est-elle autorisé ? Period ? Localement ? À distance ?
+//Back up local -> où mettre la backup ?
+//Back up externe -> récuperer : domaine, login, mdp et dossier.
+?>
