eb9f505a61d4e63c75a517c79387065302981386
Ruffenach <ruffenach.jonathan@gmail.com>
2018-06-14 18:19:33 +0200

    add extern backup in adminer

--- Adminer.php
+++ Adminer.php
@@ -1,10 +1,7 @@
 <?php
 /******Adminer******/
 
-/*Sauvegarde de la bdd*/
-//Back est-elle autorisé ? Period ? Localement ? À distance ?
-//Back up local -> où mettre la backup ?
-//Back up externe -> récuperer : domaine, login, mdp et dossier.
+/*Database Back-up*/
 
 //Reading config file
 $filename = "BackUpConfig";
@@ -12,32 +9,72 @@ $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
 
-//Extracting local backup enable
+//Extracting extern and local backup enable
+$regex_enable_extern = "#(.|\n|^)+Enable extern back-up: (True|False)(.|\n|$)+#";
 $regex_enable_local = "#(.|\n|^)+Enable local back-up: (True|False)(.|\n|$)+#";
+$enable_extern = preg_replace($regex_enable_extern, "$2", $contents);
 $enable_local = preg_replace($regex_enable_local, "$2", $contents);
 
+//Extracting period
+$regex_period = "#(.|\n|^)+Period: ([0-9]{4,})(.|\n|$)+#";
+$period = preg_replace($regex_period, "$2", $contents);
+
   //If backup enabled, extracting backup configuration
-  if ($enable_local == "True")
+  if($enable_extern == "True")
   {
-    $regex_period = "#(.|\n|^)+Period: ([0-9]{4,})(.|\n|$)+#";
-    $period = preg_replace($regex_period, "$2", $contents);
-    $regex_folder_local = "#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#";
-    $folder_local = preg_replace($regex_folder_local, "$2", $contents);
-    echo $period, "\n", $folder_local, "\n";
+    $regex_domain = "#(.|\n|^)+Domain name: ([a-z0-9._-]{2,}\.[a-z]{2,4})(.|\n|$)+#";
+    $regex_user = "#(.|\n|^)+User: (.{2,})(.|\n|$)+#";
+    $regex_password = "#(.|\n|^)+Password: (.{8,})(.|\n|$)+#";
+    $regex_folder = "#(.|\n|^)+Folder extern: (/(.+/)+)(.|\n|$)+#";
+
+    $domain = preg_replace($regex_domain, "$2", $contents);
+    $user = preg_replace($regex_user, "$2", $contents);
+    $password = preg_replace($regex_password, "$2", $contents);
+    $folder = preg_replace($regex_folder, "$2", $contents);
+
+    echo $period, " into ", $user, "@", $domain, ":", $folder, " with ", $password, "\n";
+
+
+      //Making an extern Backup
+      if( $period > 42 )
+      {
+        //creating backup();
+        //scp to $user, "@", $domain, ":", $folder, " with ", $password;
+
+        if($enable_local == "True")
+        {
+          $regex_folder = "#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#";
+          $folder = preg_replace($regex_folder, "$2", $contents);
+          //move file to folder;
+        }
+        else
+        {
+          //remove file;
+        }
+      }
+
+  }
+  else
+  {
+    echo "no extern back up\n";
+
+    if($enable_local == "True")
+    {
+      $regex_folder = "#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#";
+      $folder = preg_replace($regex_folder, "$2", $contents);
+      echo $period, " into ", $folder, "\n";
 
       /*
       //Making a local Backup
       if( $period > (now - last timestamp) )
       {
-        Backup to $folder_local
+        Backup to $folder
       }
       */
+    }
+    else
+    {
+      echo "no local back up\n";
+    }
   }
-  else
-  {
-    echo "no back up\n";
-  }
-
-
-
 ?>
