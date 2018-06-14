0d3d140ab7de57840260b9d19b73b52d8063c72f
Ruffenach <ruffenach.jonathan@gmail.com>
2018-06-14 15:22:41 +0200

    add local backup in adminer

--- Adminer.php
+++ Adminer.php
@@ -12,6 +12,32 @@ $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
 
+//Extracting local backup enable
+$regex_enable_local = "#(.|\n|^)+Enable local back-up: (True|False)(.|\n|$)+#";
+$enable_local = preg_replace($regex_enable_local, "$2", $contents);
+
+  //If backup enabled, extracting backup configuration
+  if ($enable_local == "True")
+  {
+    $regex_period = "#(.|\n|^)+Period: ([0-9]{4,})(.|\n|$)+#";
+    $period = preg_replace($regex_period, "$2", $contents);
+    $regex_folder_local = "#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#";
+    $folder_local = preg_replace($regex_folder_local, "$2", $contents);
+    echo $period, "\n", $folder_local, "\n";
+
+      /*
+      //Making a local Backup
+      if( $period > (now - last timestamp) )
+      {
+        Backup to $folder_local
+      }
+      */
+  }
+  else
+  {
+    echo "no back up\n";
+  }
+
 
 
 ?>
