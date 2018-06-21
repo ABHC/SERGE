1716953931b87b351546144de44def391a0ce9a3
Ruffenach <ruffenach.jonathan@gmail.com>
2018-06-21 18:51:26 +0200

    adding the backup command

--- Adminer.php
+++ Adminer.php
@@ -4,7 +4,7 @@
 /******Database Back-up********/
 
 //Reading config file
-$filename = 'permission/core_configuration.txt';
+$filename = '/home/ruffenach/Serge/SERGE/permission/core_configuration.txt';
 $handle = fopen($filename, 'r+');
 $contents = fread($handle, filesize($filename));
 
@@ -27,18 +27,32 @@ $lastBackup = preg_replace($regexLastBackup, '$2', $contents);
     $regexDomain = '#(.|\n|^)+Domain name: ([a-z0-9._-]{2,}\.[a-z]{2,4})(.|\n|$)+#';
     $regexUser = '#(.|\n|^)+User: (.{2,})(.|\n|$)+#';
     $regexPassword = '#(.|\n|^)+Password: (.{8,})(.|\n|$)+#';
-    $regexFolder = '#(.|\n|^)+Folder extern: (/(.+/)+)(.|\n|$)+#';
+    $regexDatabasePassword = '#(.|\n|^)+Database Password: (.{8,})(.|\n|$)+#';
+    $regexExternFolder = '#(.|\n|^)+Folder extern: (/(.+/)+)(.|\n|$)+#';
 
     $domain = preg_replace($regexDomain, '$2', $contents);
     $user = preg_replace($regexUser, '$2', $contents);
     $password = preg_replace($regexPassword, '$2', $contents);
-    $folder = preg_replace($regexFolder, '$2', $contents);
+    $databasePassword = preg_replace($regexDatabasePassword, '$2', $contents);
+    $externFolder = preg_replace($regexExternFolder, '$2', $contents);
 
-    echo "$period into $user@$domain:$folder with $password\n";
+    echo "$period into $user@$domain:$externFolder with $password\n";
 
     /*Making an extern Backup*/
     //creating_backup();
+    $date = date('dMY');
+    $localFolder = '';
+    $backUpCommand = "/usr/bin/mysqldump -u Serge -p$databasePassword Serge > ";
+    $destination = $localFolder . "Sergedata_$date.sql";
+    $command = $backUpCommand . $destination;
+    //echo "$command\n";
+    exec($command, $returnOutput, $returnValue);
+    //echo "var dump return output\n";
+    //var_dump($returnOutput);
+    //echo "return value : $returnValue\n";
+
     //scp to $user, "@", $domain, ":", $folder, " with ", $password;
+    //ne pas oublier scp -p pour les metadonné -P pour le port
 
     //If local backup enabled
     if($enableLocal == 'True')
@@ -56,6 +70,7 @@ $lastBackup = preg_replace($regexLastBackup, '$2', $contents);
     {
       echo "No local backup\n";
       //remove backup file;
+      unlink($destination);
     }
 
     $time = time();
@@ -73,13 +88,22 @@ $lastBackup = preg_replace($regexLastBackup, '$2', $contents);
     //If local backup enabled
     if($enableLocal == 'True' && (time() - $lastBackup) > $period)
     {
-      $regexFolder = '#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#';
-      $folder = preg_replace($regexFolder, '$2', $contents);
+      $regexLocalFolder = '#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#';
+      $regexDatabasePassword = '#(.|\n|^)+Database Password: (.{8,})(.|\n|$)+#';
 
-      echo "$period into $folder\n";
+      $localFolder = preg_replace($regexLocalFolder, '$2', $contents);
+      $databasePassword = preg_replace($regexDatabasePassword, '$2', $contents);
 
       /*Making a local Backup*/
       //Backup to $folder;
+      $date = date('dMY');
+      $backUpCommand = "/usr/bin/mysqldump -u Serge -p$databasePassword Serge > ";
+      $destination = $localFolder . "Sergedata_$date.sql";
+      $command = $backUpCommand . $destination;
+      exec($command, $returnOutput, $returnValue);
+      //echo "var dump return output\n";
+      //var_dump($returnOutput);
+      //echo "return value : $returnValue\n";
 
       $time = time();
       $regexLastBackup = '#(Last back-up:) ([0-9]+)#';
