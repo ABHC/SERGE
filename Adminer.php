2892acfcd00e79717193b602d54927f625bed168
Ruffenach <ruffenach.jonathan@gmail.com>
2018-06-22 16:09:05 +0200

    Cleaning code

--- Adminer.php
+++ Adminer.php
@@ -1,7 +1,14 @@
 <?php
 /******Adminer******/
 
-/******Database Back-up********/
+/******Database backup********/
+
+/********************************************/
+/*Scanner le fichier de configuration pour  */
+/*savoir si toutes les informations sont ok */
+/*et présentes ******************************/
+/*Gérer aussi les commandes des exec() si   */
+/*elles retournent des erreurs ou pas *******/
 
 //Reading config file
 $filename = '/home/ruffenach/Serge/SERGE/permission/core_configuration.txt';
@@ -9,25 +16,25 @@ $handle = fopen($filename, 'r+');
 $contents = fread($handle, filesize($filename));
 
 //Extracting extern and local backup enable
-$regexEnableExtern = '#(.|\n|^)+Enable extern back-up: (True|False)(.|\n|$)+#';
-$regexEnableLocal = '#(.|\n|^)+Enable local back-up: (True|False)(.|\n|$)+#';
+$regexEnableExtern = '#(.|\n|^)+Enable extern backup: (True|False)(.|\n|$)+#';
+$regexEnableLocal = '#(.|\n|^)+Enable local backup: (True|False)(.|\n|$)+#';
 $enableExtern = preg_replace($regexEnableExtern, '$2', $contents);
 $enableLocal = preg_replace($regexEnableLocal, '$2', $contents);
 
-//Extracting period and last back-up
+//Extracting period and last backup
 $regexPeriod = '#(.|\n|^)+Period: ([0-9]+)(.|\n|$)+#';
-$regexLastBackup = '#(.|\n|^)+Last back-up: ([0-9]{10,})(.|\n|$)+#';
+$regexLastBackup = '#(.|\n|^)+Last backup: ([0-9]{10,})(.|\n|$)+#';
 $period = preg_replace($regexPeriod, '$2', $contents);
 $lastBackup = preg_replace($regexLastBackup, '$2', $contents);
 
   //If extern backup enabled
   if($enableExtern == 'True' && (time() - $lastBackup) > $period)
   {
-    //extracting backup configuration
+    //Extracting Information about distant server
     $regexDomain = '#(.|\n|^)+Domain name: ([a-z0-9._-]{2,}\.[a-z]{2,4})(.|\n|$)+#';
     $regexUser = '#(.|\n|^)+User: (.{2,})(.|\n|$)+#';
-    $regexPassword = '#(.|\n|^)+Password: (.{8,})(.|\n|$)+#';
-    $regexDatabasePassword = '#(.|\n|^)+Database Password: (.{8,})(.|\n|$)+#';
+    $regexPassword = '#(.|\n|^)+Password: (.+)(.|\n|$)+#';
+    $regexDatabasePassword = '#(.|\n|^)+Database Password: (.+)(.|\n|$)+#';
     $regexExternFolder = '#(.|\n|^)+Folder extern: (/(.+/)+)(.|\n|$)+#';
     $regexPort = '#(.|\n|^)+Port: ([0-9]{2,4})(.|\n|$)+#';
 
@@ -38,42 +45,44 @@ $lastBackup = preg_replace($regexLastBackup, '$2', $contents);
     $externFolder = preg_replace($regexExternFolder, '$2', $contents);
     $port = preg_replace($regexPort, '$2', $contents);
 
-    /*Making an extern Backup*/
+    //Creating the name of the backup
     $date = date('dMY');
     $backUpName = "Sergedata_$date.sql";
+
+    //Building the backup command
     $backUpCommand = "/usr/bin/mysqldump -u Serge -p$databasePassword Serge > ";
     $command = $backUpCommand . $backUpName;
-    exec($command, $returnOutput, $returnValue);
-    echo "$command\n";
 
-    //scp to $user, "@", $domain, ":", $folder, " with ", $password;
-    //ne pas oublier scp -p pour les metadonné -P pour le port
+    //Creating the backup file
+    exec($command, $returnOutput, $returnValue);
+
+    //Connecting to the distant server
     $connection = ssh2_connect($domain, $port);
     ssh2_auth_password($connection, $user, $password);
+
+    //Send the backup file to the distant server
     $destination = $externFolder . $backUpName;
     ssh2_scp_send($connection, $backUpName, $destination, 0644);
 
-    //If local backup enabled
-    //move backup file to $localFolder
+    //If local backup enabled move the backup file to local destination
     if($enableLocal == 'True')
     {
+      //Extracting the local destination
       $regexLocalFolder = '#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#';
       $localFolder = preg_replace($regexLocalFolder, '$2', $contents);
 
       $destination = $localFolder . $backUpName;
       rename($backUpName, $destination);
-      echo "$destination\n";
     }
-    //If local backup not enabled
+    //If local backup not enabled remove backup file
     else
     {
-      echo "No local backup\n";
-      //remove backup file;
       unlink($backUpName);
     }
 
+    //Updating the last backup fields of the core configuration file
     $time = time();
-    $regexLastBackup = '#(Last back-up:) ([0-9]+)#';
+    $regexLastBackup = '#(Last backup:) ([0-9]+)#';
     $contents = preg_replace($regexLastBackup, "$1 $time", $contents);
     rewind($handle);
     fwrite($handle, $contents);
@@ -82,38 +91,34 @@ $lastBackup = preg_replace($regexLastBackup, '$2', $contents);
   //If extern backup not enabled
   else
   {
-    echo "no extern back up\n";
-
     //If local backup enabled
     if($enableLocal == 'True' && (time() - $lastBackup) > $period)
     {
+      //Extracting destination folder and database password
       $regexLocalFolder = '#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#';
-      $regexDatabasePassword = '#(.|\n|^)+Database Password: (.{8,})(.|\n|$)+#';
-
+      $regexDatabasePassword = '#(.|\n|^)+Database Password: (.+)(.|\n|$)+#';
       $localFolder = preg_replace($regexLocalFolder, '$2', $contents);
       $databasePassword = preg_replace($regexDatabasePassword, '$2', $contents);
 
-      /*Making a local Backup*/
-      //Backup to $folder;
+      //Creating the name of the backup
       $date = date('dMY');
       $backUpName = "Sergedata_$date.sql";
       $destination = $localFolder . $backUpName;
+
+      //Building the backup command
       $backUpCommand = "/usr/bin/mysqldump -u Serge -p$databasePassword Serge > ";
       $command = $backUpCommand . $destination;
-      exec($command, $returnOutput, $returnValue);
-      echo "$command\n";
 
+      //Creating the backup file
+      exec($command, $returnOutput, $returnValue);
+
+      //Updating the last backup fields of the core configuration file
       $time = time();
-      $regexLastBackup = '#(Last back-up:) ([0-9]+)#';
+      $regexLastBackup = '#(Last backup:) ([0-9]+)#';
       $contents = preg_replace($regexLastBackup, "$1 $time", $contents);
       rewind($handle);
       fwrite($handle, $contents);
       fclose($handle);
-    }
-    //If local backup not enabled
-    else
-    {
-      echo "no local back up\n";
     }
   }
 ?>
