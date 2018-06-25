b5d90bae102edb09544827d93f661a66116d246e
Ruffenach <ruffenach.jonathan@gmail.com>
2018-06-25 16:58:28 +0200

    Adding error management when reading configuration file

--- Adminer.php
+++ Adminer.php
@@ -15,63 +15,61 @@ $filename = '/home/ruffenach/Serge/SERGE/permission/core_configuration.txt';
 $handle = fopen($filename, 'r+');
 $contents = fread($handle, filesize($filename));
 
-//Extracting extern and local backup enable
-$regexEnableExtern = '#(.|\n|^)+Enable extern backup: (True|False)(.|\n|$)+#';
-$regexEnableLocal = '#(.|\n|^)+Enable local backup: (True|False)(.|\n|$)+#';
-$enableExtern = preg_replace($regexEnableExtern, '$2', $contents);
-$enableLocal = preg_replace($regexEnableLocal, '$2', $contents);
+$regex = array(
+  'enableExtern'     => '#(.|\n|^)+Enable extern backup: (True|False)(.|\n|$)+#',
+  'enableLocal'      => '#(.|\n|^)+Enable local backup: (True|False)(.|\n|$)+#',
+  'databasePassword' => '#(.|\n|^)+Database Password: (.+)(.|\n|$)+#',
+  'period'           => '#(.|\n|^)+Period: ([0-9]+)(.|\n|$)+#',
+  'lastBackUp'       => '#(.|\n|^)+Last backup: ([0-9]{10,})(.|\n|$)+#',
+  'localFolder'      => '#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#',
+  'domain'           => '#(.|\n|^)+Domain name: ([a-z0-9._-]{2,}\.[a-z]{2,4})(.|\n|$)+#',
+  'port'             => '#(.|\n|^)+Port: ([0-9]{2,5})(.|\n|$)+#',
+  'user'             => '#(.|\n|^)+User: (.{2,})(.|\n|$)+#',
+  'password'         => '#(.|\n|^)+Password: (.+)(.|\n|$)+#',
+  'externFolder'     => '#(.|\n|^)+Folder extern: (/(.+/)+)(.|\n|$)+#'
+);
+$error = FALSE;
+$configuration = array();
 
-//Extracting period and last backup
-$regexPeriod = '#(.|\n|^)+Period: ([0-9]+)(.|\n|$)+#';
-$regexLastBackup = '#(.|\n|^)+Last backup: ([0-9]{10,})(.|\n|$)+#';
-$period = preg_replace($regexPeriod, '$2', $contents);
-$lastBackup = preg_replace($regexLastBackup, '$2', $contents);
+foreach($regex as $fields => $value)
+{
+  if( preg_match($value, $contents, $matches) )
+  {
+    $configuration[$fields] = $matches[2];
+  }
+  else
+  {
+    echo "ERROR: Value in $fields is not correct\n";
+    $error = TRUE;
+  }
+}
 
   //If extern backup enabled
-  if($enableExtern == 'True' && (time() - $lastBackup) > $period)
+  if($configuration['enableExtern'] == 'True' && (time() - $configuration['lastBackUp']) > $configuration['period'] && $error == FALSE)
   {
-    //Extracting Information about distant server
-    $regexDomain = '#(.|\n|^)+Domain name: ([a-z0-9._-]{2,}\.[a-z]{2,4})(.|\n|$)+#';
-    $regexUser = '#(.|\n|^)+User: (.{2,})(.|\n|$)+#';
-    $regexPassword = '#(.|\n|^)+Password: (.+)(.|\n|$)+#';
-    $regexDatabasePassword = '#(.|\n|^)+Database Password: (.+)(.|\n|$)+#';
-    $regexExternFolder = '#(.|\n|^)+Folder extern: (/(.+/)+)(.|\n|$)+#';
-    $regexPort = '#(.|\n|^)+Port: ([0-9]{2,4})(.|\n|$)+#';
-
-    $domain = preg_replace($regexDomain, '$2', $contents);
-    $user = preg_replace($regexUser, '$2', $contents);
-    $password = preg_replace($regexPassword, '$2', $contents);
-    $databasePassword = preg_replace($regexDatabasePassword, '$2', $contents);
-    $externFolder = preg_replace($regexExternFolder, '$2', $contents);
-    $port = preg_replace($regexPort, '$2', $contents);
-
     //Creating the name of the backup
     $date = date('dMY');
     $backUpName = "Sergedata_$date.sql";
 
     //Building the backup command
-    $backUpCommand = "/usr/bin/mysqldump -u Serge -p$databasePassword Serge > ";
+    $backUpCommand = '/usr/bin/mysqldump -u Serge -p' . $configuration['databasePassword'] . ' Serge > ';
     $command = $backUpCommand . $backUpName;
 
     //Creating the backup file
     exec($command, $returnOutput, $returnValue);
 
     //Connecting to the distant server
-    $connection = ssh2_connect($domain, $port);
-    ssh2_auth_password($connection, $user, $password);
+    $connection = ssh2_connect($configuration['domain'], $configuration['port']);
+    ssh2_auth_password($connection, $configuration['user'], $configuration['password']);
 
     //Send the backup file to the distant server
-    $destination = $externFolder . $backUpName;
+    $destination = $configuration['externFolder'] . $backUpName;
     ssh2_scp_send($connection, $backUpName, $destination, 0644);
 
     //If local backup enabled move the backup file to local destination
-    if($enableLocal == 'True')
+    if($configuration['enableLocal'] == 'True')
     {
-      //Extracting the local destination
-      $regexLocalFolder = '#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#';
-      $localFolder = preg_replace($regexLocalFolder, '$2', $contents);
-
-      $destination = $localFolder . $backUpName;
+      $destination = $configuration['localFolder'] . $backUpName;
       rename($backUpName, $destination);
     }
     //If local backup not enabled remove backup file
@@ -92,21 +90,15 @@ $lastBackup = preg_replace($regexLastBackup, '$2', $contents);
   else
   {
     //If local backup enabled
-    if($enableLocal == 'True' && (time() - $lastBackup) > $period)
+    if($configuration['enableLocal'] == 'True' && (time() - $configuration['lastBackUp']) > $configuration['period'] && $error == FALSE)
     {
-      //Extracting destination folder and database password
-      $regexLocalFolder = '#(.|\n|^)+Folder local: (/(.+/)+)(.|\n|$)+#';
-      $regexDatabasePassword = '#(.|\n|^)+Database Password: (.+)(.|\n|$)+#';
-      $localFolder = preg_replace($regexLocalFolder, '$2', $contents);
-      $databasePassword = preg_replace($regexDatabasePassword, '$2', $contents);
-
       //Creating the name of the backup
       $date = date('dMY');
       $backUpName = "Sergedata_$date.sql";
-      $destination = $localFolder . $backUpName;
+      $destination = $configuration['localFolder'] . $backUpName;
 
       //Building the backup command
-      $backUpCommand = "/usr/bin/mysqldump -u Serge -p$databasePassword Serge > ";
+      $backUpCommand = '/usr/bin/mysqldump -u Serge -p' . $configuration['databasePassword'] . ' Serge > ';
       $command = $backUpCommand . $destination;
 
       //Creating the backup file
@@ -121,4 +113,5 @@ $lastBackup = preg_replace($regexLastBackup, '$2', $contents);
       fclose($handle);
     }
   }
+
 ?>
