841444c6553ddeef8359b518674e4081dfebcc04
Ruffenach <ruffenach.jonathan@gmail.com>
2018-08-10 18:25:37 +0200

    standby

--- delete_user.php
+++ delete_user.php
@@ -5,13 +5,16 @@ include('web/model/read.php');
 include('web/model/update.php');
 
 $tableName = 'users_table_serge';
-$fieldToRead = 'id,signup_date,email_validation';
+$fieldToRead = 'id,signup_date,email_validation,req_for_del';
 $readDatabase = read($tableName, $fieldToRead, array(), '', $bdd);
 
 foreach($readDatabase as $value)
 {
   //48 hours * 3600 = 172800 seconds
-  if( (time() - $value['signup_date'] > 172800) && $value['email_validation'] == 0 )
+  if( ( (time() - $value['signup_date'] > 172800) &&
+      $value['email_validation'] == 0 ) ||
+      (time() - $value['req_for_del'] > 172800)
+    )
   {
     $queryDeleteLine = "DELETE FROM $tableName WHERE id = ".$value['id'];
 
