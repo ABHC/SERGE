8d9b68b508749b53b772ef482042d6ffa592f14b
Ruffenach <ruffenach.jonathan@gmail.com>
2018-08-13 15:25:16 +0200

    Adding comments in delete_user file

--- delete_user.php
+++ delete_user.php
@@ -4,6 +4,7 @@ include('permission/connection_sql.php');
 include('web/model/read.php');
 include('web/model/update.php');
 
+//Reading database
 $tableName = 'users_table_serge';
 $fieldToRead = 'id,signup_date,email_validation,req_for_del';
 $readDatabase = read($tableName, $fieldToRead, array(), '', $bdd);
@@ -11,14 +12,18 @@ $readDatabase = read($tableName, $fieldToRead, array(), '', $bdd);
 foreach($readDatabase as $row)
 {
   //48 hours * 3600 = 172800 seconds
+  //If no email validation more than 48 hours OR request for deletion more than 48 hours
+  //Then delete
   if(
       ( ($row['email_validation'] == 0) && (time() - $row['signup_date']) > 172800 ) ||
       ( ($row['req_for_del'] != NULL)   && (time() - $row['req_for_del']) > 172800 )
     )
   {
 
+    //Building query
     $queryDeleteLine = "DELETE FROM $tableName WHERE id = ".$row['id'];
 
+    //Executing query
     try
     {
       $req = $bdd->prepare($queryDeleteLine);
@@ -32,5 +37,4 @@ foreach($readDatabase as $row)
 
   }
 }
-
 ?>
