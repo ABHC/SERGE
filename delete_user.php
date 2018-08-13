a892a30a8462249e5d8ac50a63c792ca3dd593a1
Ruffenach <ruffenach.jonathan@gmail.com>
2018-08-13 15:14:51 +0200

    adding complete user deletion function in delete_user file

--- delete_user.php
+++ delete_user.php
@@ -8,15 +8,16 @@ $tableName = 'users_table_serge';
 $fieldToRead = 'id,signup_date,email_validation,req_for_del';
 $readDatabase = read($tableName, $fieldToRead, array(), '', $bdd);
 
-foreach($readDatabase as $value)
+foreach($readDatabase as $row)
 {
   //48 hours * 3600 = 172800 seconds
-  if( ( (time() - $value['signup_date'] > 172800) &&
-      $value['email_validation'] == 0 ) ||
-      (time() - $value['req_for_del'] > 172800)
+  if(
+      ( ($row['email_validation'] == 0) && (time() - $row['signup_date']) > 172800 ) ||
+      ( ($row['req_for_del'] != NULL)   && (time() - $row['req_for_del']) > 172800 )
     )
   {
-    $queryDeleteLine = "DELETE FROM $tableName WHERE id = ".$value['id'];
+
+    $queryDeleteLine = "DELETE FROM $tableName WHERE id = ".$row['id'];
 
     try
     {
@@ -29,11 +30,6 @@ foreach($readDatabase as $value)
       error_log($e->getMessage(), 0);
     }
 
-    echo "line deleted\n";
-  }
-  else
-  {
-    echo "no line deleted\n";
   }
 }
 
